41af43db5b466dc9876309d2f050db19bdd97d97
Oleksii Mikhno <xioflesh@gmail.com>
2022-12-29 09:25:45 +0200

    first init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import './style/scss/default.scss';
+
+import './js/bootstrap/bootstrap';
+
+// import handlerForm from './js/handlerForm';
+
+// handlerForm();
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCss = require('mini-css-extract-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+
+const APPDIR = 'src/';
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    watch: true,
+    devServer: {
+        watchFiles: ["./src/*"],
+        static: './dist',
+        port: 8080,
+        hot: true,
+    },
+    module: {
+        rules: [{
+            test:/\.(s*)css$/,
+            use: [
+               miniCss.loader,
+               'css-loader',
+               'sass-loader',
+            ]
+        }],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "MapStars",
+            filename: 'index.html',
+            template: path.resolve(__dirname, APPDIR, 'index.html'),
+            chunks: ['main']
+        }),
+        new miniCss({
+            filename: './style/default.css',
+        })
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        // publicPath: '/'
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+}
